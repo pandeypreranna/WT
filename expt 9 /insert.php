911f6108e506a30e4cad5daa924985b9daa47dfd
pandeypreranna <prerannapandey20@gmail.com>
2026-04-19 14:05:45 +0530

    Update insert.php

--- expt 9 /insert.php
+++ expt 9 /insert.php
@@ -1,1 +1,18 @@
+<?php
+include 'db.php';
 
+$name = $_POST['name'];
+$email = $_POST['email'];
+$age = $_POST['age'];
+
+$sql = "INSERT INTO users (name, email, age)
+VALUES ('$name', '$email', '$age')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Data inserted successfully!";
+} else {
+    echo "Error: " . $conn->error;
+}
+
+$conn->close();
+?>
