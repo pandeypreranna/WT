15ca8a48cd53dbf345f11e3999a703c3b57d8b3e
pandeypreranna <prerannapandey20@gmail.com>
2026-04-19 14:06:47 +0530

    Update index.php

--- expt 9 /index.php
+++ expt 9 /index.php
@@ -1,1 +1,16 @@
+<!DOCTYPE html>
+<html>
+<body>
 
+<h2>User Form</h2>
+
+<form action="insert.php" method="POST">
+    Name: <input type="text" name="name"><br><br>
+    Email: <input type="email" name="email"><br><br>
+    Age: <input type="number" name="age"><br><br>
+
+    <input type="submit" value="Submit">
+</form>
+
+</body>
+</html>
