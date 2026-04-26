7b746907c698c96a78fa6fbffe58b2085b56dc6b
pandeypreranna <prerannapandey20@gmail.com>
2026-04-26 21:55:20 +0530

    Update script.js

--- Project/assets/script.js
+++ Project/assets/script.js
@@ -1,1 +1,7 @@
-
+function validateForm() {
+    let title = document.forms["itemForm"]["title"].value;
+    if (title == "") {
+        alert("Title must be filled out");
+        return false;
+    }
+}
